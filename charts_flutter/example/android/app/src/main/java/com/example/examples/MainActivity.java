337030852b09b95a40bd6c9d9167d4c87c194e18
Ihor Terletskyi <igor.terletskij@privatbank.ua>
2023-09-01 02:24:52 +0300

    add fixes

--- charts_flutter/example/android/app/src/main/java/com/example/examples/MainActivity.java
+++ charts_flutter/example/android/app/src/main/java/com/example/examples/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.examples;
 
 import android.os.Bundle;
-
-import io.flutter.app.FlutterActivity;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+
+import static io.flutter.embedding.android.KeyData.CHANNEL;
+
 
 /**
  * FlutterActivity
  */
 public class MainActivity extends FlutterActivity {
+
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                    (call, result) -> {
+                        // Your existing code
+                }
+        );
+    }
 }
